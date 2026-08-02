e8aae491bbc7397df6d672ffc632ea8a82fd2302
Admin密码0000 <Admin@MacBook-Air.local>
2026-08-02 14:10:50 +0800

    feat: complete case content and map experience

---
+++ src/data/case-details.js
@@ -0,0 +1,7 @@
+import { caseDetails as details1 } from './case-details-1.js';
+import { caseDetails as details2 } from './case-details-2.js';
+import { caseDetails as details3 } from './case-details-3.js';
+import { caseDetails as details4 } from './case-details-4.js';
+
+export const caseDetails = { ...details1, ...details2, ...details3, ...details4 };
+
